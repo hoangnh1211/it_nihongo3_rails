c1fbecd10655bad8b39fabfbe0fc60efd95c0868
hoangnh1211 <vuminhhoanganh1211@gmail.com>
2020-11-22 15:34:49 +0000

    add_home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :books
-=======
+  # get 'static_pages/home'
+  root 'static_pages#home'
   devise_for :users
   devise_for :models
->>>>>>> add_login
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
